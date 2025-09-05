6fe1f92ae439cb9a8a33537acfe8e5213a3fe755
Mathilde Arconte <signup@marconte.fr>
2025-09-05 12:20:33 +0200

    Add ACF group for subtitle and update page template to display it
    - Created a new ACF group for the "Subtitle page" with a WYSIWYG field for subtitles.
    - Updated the page.php template to retrieve and display the subtitle above the page content.

---
+++ page.php
@@ -0,0 +1,29 @@
+	<?php get_header();
+	$subtitle = get_field('subtitle');
+	?>
+	<div class="page-header">
+		<div class="container">
+			<div class="row">
+				<div class="col col-md-8">
+					<h1><?php the_title(); ?></h1>
+					<?php if ($subtitle): ?>
+						<?php echo $subtitle; ?>
+					<?php endif; ?>
+				</div>
+				<div class="col col-md-4">
+					<?php if (has_post_thumbnail()): ?>
+						<div class="page-thumbnail">
+							<?php the_post_thumbnail('medium'); ?>
+						</div>
+					<?php endif; ?>
+				</div>
+			</div>
+
+		</div>
+	</div>
+
+	<div class="">
+		<?php the_content(); ?>
+	</div>
+
+	<?php get_footer(); ?>
