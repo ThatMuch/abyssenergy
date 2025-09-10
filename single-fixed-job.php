b69171a4528e33f2726ed2ca88c758aad9e53ac3
Mathilde Arconte <signup@marconte.fr>
2025-09-10 16:56:22 +0200

    feat: Réorganisation du code pour le bloc Job Detail avec ajout de styles et gestion des sous-titres

--- single-fixed-job.php
+++ single-fixed-job.php
@@ -1,55 +1,44 @@
-<?php get_header(); ?>
+<?php get_header();
+$subtitle = get_field('subtitle');
+$description = get_field('description');
+?>
 
 
-
-<section class='content job-detail-fixed'>
-    
-    <div class="wrapper">
-    
-        <?php if (have_posts()) : ?>
-        
-        <?php while (have_posts()) : the_post(); ?>
-        
-        <div id="jobsearchcta" class="wp-block-group has-colour-white-color has-colour-orange-background-color has-text-color has-background has-link-color wp-elements-0856c7e6f7a84ee3058578ca6cd86a50 showOnScroll jobcta"><div class="wp-block-group__inner-container is-layout-constrained wp-block-group-is-layout-constrained">
-<p class="has-colour-white-color has-colour-orange-background-color has-text-color has-background has-link-color wp-elements-c68677137f738a803b62161fcc92fcc6">Didn’t find your dream job?<br /><a href="https://abyssenergy-fr.squarechilli-staging.co.uk/contact-us/?position=Unsolicited%20Application" data-type="page" data-id="38">Send us your CV</a></p>
-</div></div>
-        
-        <h1 class="wp-block-heading has-colour-orange-color"><?php the_title(); ?></h1>
-        
-        <div>
-                
-            <h2 style='margin-top: 28px;padding-bottom: 0'>Job Description</h2>
-
-            <?php the_content(); ?>
-
-            <div class="wp-block-buttons is-layout-flex wp-container-core-buttons-is-layout-1 wp-block-buttons-is-layout-flex">
-                <div class="wp-block-button is-style-fill showtabs">
-                    <a href='/for-candidates/' class="wp-block-button__link has-colour-white-color has-colour-orange-background-color has-text-color has-background has-link-color has-text-align-center wp-element-button" style="border-radius:10px">Back to all jobs</a>
-                </div>
+<div class="page-header">
+    <div class="container">
+        <div class="row">
+            <div class="col col-md-7">
+                <h1><?php the_title(); ?></h1>
+                <?php if ($subtitle): ?>
+                    <?php echo $subtitle; ?>
+                <?php endif; ?>
+                <?php if ($description): ?>
+                    <div class="page-description">
+                        <?php echo $description; ?>
+                    </div>
+                <?php endif; ?>
             </div>
-            
-            <br style='margin-bottom: 50px' />
-                
-               
-                
+            <div class="col col-md-5">
+                <?php if (has_post_thumbnail()): ?>
+                    <div class="page-thumbnail">
+                        <?php the_post_thumbnail('medium'); ?>
+                    </div>
+                <?php endif; ?>
             </div>
+        </div>
 
-            
-        
-        
-        
-        
-
-        <?php endwhile; ?>
-
-        <?php else : ?>
-
-
-
-        <?php endif; ?>
-        
     </div>
-    
-</section>
+</div>
 
-<?php get_footer(); ?> 
+<div class='container'>
+    <?php if (have_posts()) : ?>
+        <?php while (have_posts()) : the_post(); ?>
+            <div>
+                <h2 style='margin-top: 28px;padding-bottom: 0'>Job Description</h2>
+                <?php the_content(); ?>
+            </div>
+        <?php endwhile; ?>
+    <?php endif; ?>
+</div>
+
+<?php get_footer(); ?>
